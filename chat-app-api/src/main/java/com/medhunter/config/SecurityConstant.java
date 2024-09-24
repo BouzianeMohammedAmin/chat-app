1e3826659831b3bbfd6e2b5767318d5905a16899
Bouziane Mohammed Amin <itsbouzianemed@gmail.com>
2024-09-24 17:26:31 +0100

    add  bassics config security with jwt

---
+++ chat-app-api/src/main/java/com/medhunter/config/SecurityConstant.java
@@ -0,0 +1,9 @@
+package com.medhunter.config;
+
+public class SecurityConstant {
+
+    public static final String JWT_KEY="kzjjbeiurbZGyurZvzpaqekmeecfeeljliuogcerwmqzsduphbeheb";
+    public static final String HEADER = "Authorization";
+
+
+}
